d4a9267aa404b4793564fb38a409f4909a10c95c
Anthonie CAVA <anthonie.cava@student.vinci.be>
2023-09-20 23:43:56 +0200

    1.3 : API : create & REST client

--- exercices/1.3/routes/films.js
+++ exercices/1.3/routes/films.js
@@ -34,7 +34,7 @@ router.get('/', (req, res, next) => {
     res.json(FILMS);
 });
 
-//Read the films identified by and id
+//Read the films identified by id
 router.get('/:id', (req, res) =>{
     console.log(`GET /films/${req.params.id}`);
 
@@ -46,8 +46,27 @@ router.get('/:id', (req, res) =>{
     res.json(FILMS[filmsIndex]);
 });
 
-
-
+// Read all the films, filtered by minimum-duration if the query param exists
+router.get('/', (req, res) => {
+    // Extraction du paramètre 'minimum-duration' de la requête en tant que nombre
+    const minimumFilmDuration = Number(req.query['minimum-duration']);
+  
+    // Vérification si 'minimumFilmDuration' n'est pas un nombre valide ou est inférieur ou égal à zéro
+    if (isNaN(minimumFilmDuration) || minimumFilmDuration <= 0) {
+      // Si la condition est vraie, renvoie un message d'erreur
+      return res.json('Wrong minimum duration');
+    }
+  
+    // Filtrage des films pour ne garder que ceux dont la durée est supérieure ou égale à 'minimumFilmDuration'
+    const filmsReachingMinimumDuration = films.filter(
+      (film) => film.duration >= minimumFilmDuration
+    );
+  
+    // Envoi de la liste filtrée en réponse à la requête
+    res.json(filmsReachingMinimumDuration);
+  });
+  
+  
 
 
   module.exports = router;
