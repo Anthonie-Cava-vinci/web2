80c054313c2e28f37a22688b89b68cb426943a35
Anthonie CAVA <anthonie.cava@student.vinci.be>
2023-10-16 14:55:43 +0200

    2.4.1 : run action within timer

---
+++ exercices/2.4.1/index.js
@@ -0,0 +1,33 @@
+const maxTime = 5; //x
+const expectedClicks = 10; //x
+let startTime;
+let currentClicks = 0; //x
+let timerReference;
+let timeOutID;
+
+const button = document.querySelector('button');
+const message = document.querySelector('#message');
+
+button.addEventListener('mouseenter', startCounter);
+button.addEventListener('click', clickHandler);
+
+function startCounter() {
+  startTime = new Date();
+  timerReference = setTimeout(printLoss, maxTime * 1000);
+}
+
+function clickHandler() {
+  currentClicks++;
+  if (currentClicks === expectedClicks) {
+    clearTimeout(timerReference);
+    const timeSpent = new Date().getTime() - startTime.getTime();
+    button.style.display = 'none';
+    alert(`You win ! you clicked ${expectedClicks} times within ${timeSpent}s!`);
+  }
+}
+
+function printLoss() {
+  timeOutID = setTimeout(() => {
+    alert(`Game over, you did not click ${expectedClicks} times within ${maxTime}s !`);
+  })
+}
